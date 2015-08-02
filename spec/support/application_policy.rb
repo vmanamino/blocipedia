3d381384b92ad37182a85d2d65c0563dacb34f01
Viral Amin <vmanamino@gmail.com>
2015-08-02 21:26:55 +0000

    DRYed policy tests

--- spec/support/application_policy.rb
+++ spec/support/application_policy.rb
@@ -1,27 +1,63 @@
 require 'rails_helper'
 include Devise::TestHelpers
 
-shared_examples 'application_policy' do
-  subject { ApplicationPolicy }
-  let(:user) { create(:user) }
-  let(:wiki) { create(:wiki, user: user) }
-  permissions :edit? do
-    it 'allows user owner to edit record' do
-      expect(subject).to permit(user, wiki)
-    end
-    it 'allows user to edit record of another user' do
-      expect(subject).to permit(create(:user), wiki)
-    end
+shared_examples 'application_policy_index' do
+  permissions :index? do
+    it 'does not permit view' do
+      @wiki_list = create_list(:wiki, 10)
+      expect(subject).not_to permit(@wiki_list)
+     end
   end
-  permissions :create? do
-    it 'successful when permissions match' do
-      expect(subject).to permit(user, wiki)
-    end
-  end
+end
+
+shared_examples 'application_policy_show' do
+subject { ApplicationPolicy }
+let(:user) { create(:user) }
+let(:wiki) { create(:wiki, user: user) }
   permissions :show? do
-    it 'successful show to web site visitor' do
+    it 'does permit view to visitor' do
       youser = double
       expect(subject).to permit(youser, wiki)
     end
+  end
+end
+
+shared_examples 'application_policy_create' do
+subject { ApplicationPolicy }
+let(:user) { create(:user) }
+let(:wiki) { create(:wiki, user: user) }
+  permissions :create? do
+    it 'permits User to create wiki' do
+      expect(subject).to permit(user, wiki)
+    end
+  end
+end
+
+shared_examples 'application_policy_new' do
+  permissions :new? do
+    it_behaves_like 'application_policy_create'
+  end
+end
+
+shared_examples 'application_policy_update' do
+subject { ApplicationPolicy }
+let(:user) { create(:user) }
+let(:wiki) { create(:wiki, user: user) }
+  permissions :update? do
+    it 'permits User to update wiki' do
+      expect(subject).to permit(user, wiki)
+    end
+  end
+end
+
+shared_examples 'application_policy_edit' do
+  permissions :edit? do
+    it_behaves_like 'application_policy_update'
+  end
+end
+
+shared_examples 'application_policy_destroy' do
+  permissions :destroy? do
+    it_behaves_like 'application_policy_update'
   end
 end
