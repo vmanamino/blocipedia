64125306b3486be295168452667e1ab1d5ea997b
Viral Amin <vmanamino@gmail.com>
2015-08-04 04:52:42 +0000

    modified scope

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -5,7 +5,8 @@ class Wiki < ActiveRecord::Base
   validates :body, length: { minimum: 20 }, presence: true
   validates :user, presence: true
 
-  scope :visible_to, -> (user) { user.role == 'premium' ? all : where(private: false) }
+  # scope :visible_to, -> (user) { user.role == 'premium' ? all : where(private: false) }
+  scope :visible_to, -> (user) { where('user_id=? OR private=?', user.id, false) } # truly private
 
   extend FriendlyId
   friendly_id :title, use: [:slugged, :history]
--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 describe Wiki do
   before do
+    @user = create(:user, role: 'premium')
     @wikis = create_list(:wiki, 5)
-    wiki_private = create(:wiki, private: true)
+    wiki_private = create(:wiki, user_id: @user.id, private: true)
     @wikis.push(wiki_private) # add one private to test scope
+    @user_two = create(:user, role: 'premium') # premium user to test scope
+    wiki_private_premium_owner = create(:wiki, user_id: @user_two.id, private: true) # private wiki owned by premium user
+    @wikis.push(wiki_private_premium_owner) # total is 7 wikis, 6 will be visible to @user_two
   end
   it 'has working test factory' do
     @wiki = create(:wiki)
@@ -18,12 +22,26 @@ describe Wiki do
     @wiki = create(:wiki)
     expect(@wiki.private).to be(false)
   end
-  it 'scope limits private wikis to premium users' do
-    user = create(:user, role: 'premium')
-    expect(Wiki.visible_to(user).count).to eq(6)
+  it 'scope returns private wikis that belongs to a given premium user only' do
+    wikis = Wiki.visible_to(@user_two)
+    private_wiki = wikis.where(private: true)
+    expect(private_wiki[0].user_id).to eq(@user_two.id)
+    wikis = Wiki.visible_to(@user)
+    private_wiki = wikis.where(private: true)
+    expect(private_wiki[0].user_id).not_to eq(@user_two.id)
+    expect(private_wiki[0].user_id).to eq(@user.id)
   end
-  it 'scope keeps private wikis from standard users' do
-    user = create(:user)
-    expect(Wiki.visible_to(user).count).to eq(5)
+  it 'scope returns all public wikis to a given premium user' do
+    wikis = Wiki.visible_to(@user_two)
+    public_wikis = wikis.where(private: false)
+    expect(public_wikis.count).to eq(5)
   end
+#   it 'scope limits private wikis to premium users' do
+#     user = create(:user, role: 'premium')
+#     expect(Wiki.visible_to(user).count).to eq(6)
+#   end
+#   it 'scope keeps private wikis from standard users' do
+#     user = create(:user)
+#     expect(Wiki.visible_to(user).count).to eq(5)
+#   end
 end
