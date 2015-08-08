091bcc917d9c64bd11f423407002debe9ed60221
Viral Amin <vmanamino@gmail.com>
2015-08-08 05:42:28 +0000

    wiki form partial, modifying policies

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -2,11 +2,7 @@ class WikiPolicy < ApplicationPolicy
   def index?
     true
   end
-
-  def show?
-    user.present? && (user.premium? || user.admin?)
-  end
-
+  
   def destroy?
     user.present? && (record.user == user || user.admin?)
   end
