c267a909f3abe05727b4a6806e3862069c4ac74b
Viral Amin <vmanamino@gmail.com>
2015-08-08 07:56:10 +0000

    modified scope for site vistors

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -6,7 +6,7 @@ class Wiki < ActiveRecord::Base
   validates :user, presence: true
 
   # scope :visible_to, -> (user) { user.role == 'premium' ? all : where(private: false) }
-  scope :visible_to, -> (user) { where('user_id=? OR private=?', user.id, false) } # truly private
+  scope :visible_to, -> (user) { user ? where('user_id=? OR private=?', user.id, false) : where(private: false) } # truly private
 
   extend FriendlyId
   friendly_id :title, use: [:slugged, :history]
