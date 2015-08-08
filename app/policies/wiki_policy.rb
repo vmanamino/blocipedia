76c5dcf81e5d8e13cdc6222938f6e5a90cd90d7d
Viral Amin <vmanamino@gmail.com>
2015-08-08 18:53:22 +0000

    modified wiki policy with tests

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -2,7 +2,12 @@ class WikiPolicy < ApplicationPolicy
   def index?
     true
   end
-
+  def show?
+    record.private == false || (user.admin? || record.user == user)
+  end
+  def update?
+    user.present? && (record.private == false || (user.admin? || record.user == user))
+  end
   def destroy?
     user.present? && (record.user == user || user.admin?)
   end
--- spec/policies/wiki_policy.rb
+++ spec/policies/wiki_policy.rb
@@ -16,21 +16,38 @@ describe WikiPolicy do
     end
   end
   permissions :show? do
-    it_behaves_like 'application_policy_show'
-#     before do
-#       @private_wiki = create(:wiki, private: true)
-#     end
-#     it 'permits view to premium user' do
-#       user = create(:user, role: 'premium')
-#       expect(subject).to permit(user, @private_wiki)
-#     end
-#     it 'permits view to admin user' do
-#       user = create(:user, role: 'admin')
-#       expect(subject).to permit(user, @private_wiki)
-#     end
-#     it 'denies view to standard user' do
-#       expect(subject).not_to permit(user, @private_wiki)
-    #     end # rubocop:disable Style/CommentIndentation
+    before do
+      @premium_user_owner = create(:user, role: 'premium')
+      @private_wiki = create(:wiki, private: true, user: @premium_user_owner)
+      @premium_user_other = create(:user, role: 'premium')
+      @standard_user_other = create(:user)
+      @admin_user = create(:user, role: 'admin')
+      @visitor = double
+    end
+    it 'permits view of private wiki to premium user owner' do
+      expect(subject).to permit(@premium_user_owner, @private_wiki)
+    end
+    it 'denies view of private wiki to another premium user' do
+      expect(subject).not_to permit(@premium_user_other, @private_wiki)
+    end
+    it 'denies view of private wiki to a given standard user' do
+      expect(subject).not_to permit(user, @private_wiki)
+    end
+    it 'permits view of public wiki to a given premium user' do
+      expect(subject).to permit(@premium_user_owner, wiki)
+    end
+    it 'permits view of public wiki to a given standard user' do
+      expect(subject).to permit(user, wiki)
+    end
+    it 'permits view of private wiki to a given admin user' do
+      expect(subject).to permit(@admin_user, @private_wiki)
+    end
+    it 'permits view of public wiki to a given admin user' do
+      expect(subject).to permit(@admin_user, wiki)
+    end
+    it 'permits view of public wiki to a site visitor' do
+      expect(subject).to permit(@visitor, wiki)
+    end
   end
   permissions :create? do
     it_behaves_like 'application_policy_create'
@@ -39,7 +56,21 @@ describe WikiPolicy do
     it_behaves_like 'application_policy_new'
   end
   permissions :update? do
-    it_behaves_like 'application_policy_update'
+    before do
+      @premium_user_owner = create(:user, role: 'premium')
+      @private_wiki = create(:wiki, private: true, user: @premium_user_owner)
+      @premium_user_other = create(:user, role: 'premium')
+      @standard_user_other = create(:user)
+      @admin_user = create(:user, role: 'admin')
+    end
+    it 'permits user owner to update wiki' do
+      expect(subject).to permit(@premium_user_owner, @private_wiki)
+      expect(subject).to permit(user, wiki)
+    end
+    it 'denies updating wiki to another user' do
+      expect(subject).not_to permit(@standard_other_user, wiki)
+      expect(subject).not_to permit(@premium_user_other, @private_wiki)
+    end
   end
   permissions :edit? do
     it_behaves_like 'application_policy_edit'
